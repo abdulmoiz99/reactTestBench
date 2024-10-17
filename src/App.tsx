300d5484d117887c0583b1695d4e7ac0e372b469
Abdul Moiz <moiza0799@gmail.com>
2024-10-17 16:19:17 -0500

    Implemented Controlled Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import './App.css'
-import { Greetings } from './Greetings'
 
 function App() {
-  const [message, setMessage] = useState("Hello Abdul Moiz")
+  const [formData, setFormData] = useState({ username: "", password: "" })
 
+  const submitForm = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(formData)
+  }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({ ...prevState, [name]: value }));
+  }
   return (
     <>
-      <Greetings message={message} />
-      <button onClick={() => setMessage("Hello From the Button")}> Update</button>
+      <form onSubmit={submitForm}>
+        <p>Username:
+          <input
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </p>
+        <p>Password:
+          <input
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </p>
+        <button type='submit'>Submit</button>
+      </form>
     </>
   )
 }
