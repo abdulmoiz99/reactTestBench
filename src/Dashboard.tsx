5d3c0c69691a78cf3ccfd7d75df80a33f81cc7a2
Abdul Moiz <moiza0799@gmail.com>
2024-10-17 15:47:27 -0500

    Code changes

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,7 +1,11 @@
+import { useDebugValue } from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 
+
 export const Dashboard = () => {
+    const isOnline: boolean = false;
+    useDebugValue(isOnline ? 'Online' : 'Offline');
     return (
         <>
             <Navbar />
