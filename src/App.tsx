63a8fd4131799dc7892829a9af249f9de28cd206
Abdul Moiz <moiza0799@gmail.com>
2024-10-17 19:38:35 -0500

    parent to child communication

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 // App.tsx
-import { useReducer } from 'react';
 import './App.css';
-import { ReducerComponent } from './ReducerComponent';
-import { reducer, State } from './reducer';
+import { Parent } from './parentToChild/parent';
 
 function App() {
-  const initialState: State = { count: 0 };
-  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
-      <h2>Count: {state.count}</h2>
-      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
-      <br /><br />
-      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
-      <br /><br />
-      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
-      <ReducerComponent />
+      <Parent />
     </>
   );
 }
